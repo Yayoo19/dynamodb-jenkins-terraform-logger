0f73e76096979b26471c9813732115f3fd7eb546
Jorge Chavez <chavezjorgeeduardo19@gmail.com>
2024-08-21 10:00:55 -0600

    handler corrections

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,10 +3,10 @@ import boto3
 from datetime import datetime
 import uuid
 
-def main():
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('LogsTable')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('LogsTable')
 
+def lambda_handler(event, context):
     log_message = event.get('message', 'Jenkins build has been executed')
     timestamp = datetime.now().isoformat()
     log_id = str(uuid.uuid4())
@@ -23,6 +23,3 @@ def main():
         'statusCode': 200,
         'body': json.dumps(f'Log entry added with ID: {log_id}')
     }
-
-def lambda_handler(event, context):
-    main()
