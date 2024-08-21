4e2e57d79ee2bd36679ff5b74d631e6ba11b6459
Jorge Chavez <chavezjorgeeduardo19@gmail.com>
2024-08-21 10:14:13 -0600

    Incremental integer as table

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -5,11 +5,30 @@ import uuid
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('LogsTable')
+counter_table = dynamodb.Table('LogIdCounter')
+
+def get_next_log_id():
+    try:
+        response = counter_table.get_item(Key={'id': 'counter'})
+        current_id = response.get('Item', {}).get('current_id', 0)
+        
+        next_id = current_id + 1
+        
+        counter_table.update_item(
+            Key={'id': 'counter'},
+            UpdateExpression='SET current_id = :val',
+            ExpressionAttributeValues={':val': next_id}
+        )
+        
+        return next_id
+    except ClientError as e: # type: ignore
+        print(f"Error getting or updating LogIdCounter: {e}")
+        raise
 
 def lambda_handler(event, context):
     log_message = event.get('message', 'Jenkins build has been executed')
     timestamp = datetime.now().isoformat()
-    log_id = str(uuid.uuid4())
+    log_id = get_next_log_id()
 
     item = {
         'logId': log_id,
